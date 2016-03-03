0ea8318ff3fca6580048c31fd24b3f6e94ae7b2f
Drew Banin <drew@8tracks.com>
2016-03-03 15:42:51 -0500

    add score calc

---
+++ fantasy_score.sql
@@ -0,0 +1,33 @@
+
+select 
+*,
+(points * 1) + (field_goal_3_made * 0.5) + (rebounds * 1.25) + (assists * 1.5) + (steals * 2) + (blocks * 2) + (turnovers * -0.5) + (double_double * 1.5) + (triple_double * 3) as score
+from (
+select *,
+double_score >= 2 as double_double,
+double_score >= 3 as triple_double
+from (
+select *,
+ (pts_double + reb_double + ast_double + blk_double + stl_double) as double_score
+from (
+select
+ player_name,
+ game_id,
+ team_id,
+ player_id,
+ pts as points,
+ fg3m as field_goal_3_made,
+ reb as rebounds,
+ ast as assists,
+ stl as steals,
+ blk as blocks,
+ `to` as turnovers,
+  pts >= 10 as pts_double,
+  reb >= 10 as reb_double,
+  ast >= 10 as ast_double,
+  blk >= 10 as blk_double,
+  stl >= 10 as stl_double  
+from traditional_boxscores
+) s
+)s2
+)s3 limit 1000
